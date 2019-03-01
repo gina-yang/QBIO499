f432d8359c21c84a423584142be22d3a0044e615
Gina Yang <ginayang@usc.edu>
2019-03-01 15:34:31 -0800

    Finished aov

--- HW7/HW7.R
+++ HW7/HW7.R
@@ -1,3 +1,9 @@
+# https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3531285/
+
+# The null hypothesis of an ANOVA using a single SNP is that there is no difference 
+# between the trait means of any genotype group
+
+
 getGroup <- function(toGroup, posVec){
 	grouping <- c(0)
 	count = 1
@@ -12,29 +18,43 @@ testVariance <- function(snp, trait){
 	# Split individuals based on genotype. 
 	# Each vector contains the (column) positions of these individuals
 	domPos = which(snp == "1|1") 
-	addPos = which(snp %in% c("1|0", "0|1"))
+	hetPos = which(snp %in% c("1|0", "0|1"))
 	recPos = which(snp == "0|0") 
 
 	# Group individuals based on phenotype.
 	domGroup = getGroup(trait, domPos)
-	addGroup = getGroup(trait, addPos)
+	hetGroup = getGroup(trait, hetPos)
 	recGroup = getGroup(trait, recPos)
 
-	
-	print(mean(domGroup))
-	print(mean(addGroup))
-	print(mean(recGroup))
+	g <- factor(rep(letters[1:3], c(length(domGroup), length(hetGroup), length(recGroup))),)
+	test = aov(trait~g)
+	print(summary(test))
+	pval = summary(test)[[1]][["Pr(>F)"]]
+	print(pval)
+	return(pval)
+
+
+	# print(mean(domGroup))
+	# print(mean(hetGroup))
+	# print(mean(recGroup))
+	# print("\n")
+
 }
 
 main <- function(genotypes, phenotypes){
-	for( i in range(2, 4) ){
+	for( i in 1:3 ){
 		# Get traits 
 		trait = as.vector(phenotypes[i, 10:90],mode="numeric")
-		for( j in range(1,81) ){
+		v <- c(0)
+		for( j in 1:81 ){
 			# Get SNPs
 			snp = as.vector(unlist(genotypes[j, 10:90]))
-			testVariance(snp, trait)
+			pval = testVariance(snp, trait)
+			v[j] = pval
 		}
+		print(v)
+		v = p.adjust(v)
+		print(v)
 	}
 }
 

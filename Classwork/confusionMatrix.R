3f456c2c7e17791d3005e14513572e41efc5dda7
Gina Yang <ginayang@usc.edu>
2019-03-29 00:58:21 -0700

    Added 28 Mar classwork

--- Classwork/confusionMatrix.R
+++ Classwork/confusionMatrix.R
@@ -14,3 +14,14 @@ confmx <- function(myfit, mydata, th){
 	cm[2,1] = sum(guess[truth == "Yes"] == "No", na.rm = T)
 	cm[2,2] = sum(guess[truth == "Yes"] == "Yes", na.rm = T)
 }
+
+roc -< function(myfit, mydata, thvec) {
+	tp = thvec
+	fp = thvec
+	for( i in 1:length(thvec)){
+		th = thvec[i]
+		cm = confmx(myfit, mydata, th)
+		tp[i] = cm[2,2]/sum(cm[2,])  # True positive rate (sensitivity)
+		fp[i] = cm[1,2]/sum(cm[1,])  # False positive rate (1 - specificity)
+	}
+}

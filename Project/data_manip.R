8a04ba990c69d5cdab4cac196771c90676e7ba3a
Gina Yang <ginayang@usc.edu>
2019-05-04 23:25:57 -0700

    Added commands

--- Project/data_manip.R
+++ Project/data_manip.R
@@ -5,18 +5,20 @@ lsk <- read.table("GSM3330197_LSK-CTRL.counts.txt")
 # Sample 1000 cells from lsk set
 lsk = sample(lsk, 1000)
 
-# 
-# Prune the data sets
-#
 
+
+# Prune the data frames
 col23_rn = rownames(col23)
-# Use the rownames of col123 to get those that are also in lsk
+# Use the rownames of col123 to get genes that are also in lsk
 lsk_prune <- lsk[which(rownames(lsk) %in% col23_rn),]
 lsk_rn = rownames(lsk_prune)
-# Use the rownames of the pruned lsk to get those that are also in col123
+# Use the rownames of the pruned lsk to get genes that are also in col123
 col23_prune <- col23[which(rownames(col23) %in% lsk_rn), ]
 
-# Merge the data frames
+
+# Merge (horizontal)
 df <- cbind(col23_prune, lsk_prune)
+
+write.table(TikhonovaData.txt)
 
 
